f7ef2eda5cfd925a418aba752ee3ba413721bdc9
Alex Waugh <alex@alexwaugh.com>
2002-06-28 12:27:47 +0000

    Merge changes from 1.9

--- !ccache/ccache.c
+++ !ccache/ccache.c
@@ -28,23 +28,84 @@
 #define PATH_SEPARATOR ":"
 #endif
 
+/* the base cache directory */
 char *cache_dir = NULL;
+
+/* the debug logfile name, if set */
 char *cache_logfile = NULL;
+
+/* the argument list after processing */
 static ARGS *stripped_args;
+
+/* the original argument list */
 static ARGS *orig_args;
+
+/* the output filename being compiled to */
 static char *output_file;
+
+/* the source file */
 static char *input_file;
+
+/* the name of the file containing the cached object code */
 static char *hashname;
+
+/* the extension of the file after pre-processing */
+static char *i_extension;
+
+/* the name of the temporary pre-processor file */
+static char *i_tmpfile;
+
+/* the name of the cpp stderr file */
+static char *cpp_stderr;
+
+/* the name of the statistics file */
 char *stats_file = NULL;
+
+/* did we find a -g option? */
 static int found_debug;
+
+/* a list of supported file extensions, and the equivalent
+   extension for code that has been through the pre-processor
+*/
+static struct {
+	char *extension;
+	char *i_extension;
+} extensions[] = {
+	{"c", "i"},
+	{"C", "ii"},
+	{"m", "mi"},
+	{"cc", "ii"},
+	{"CC", "ii"},
+	{"cpp", "ii"},
+	{"CPP", "ii"},
+	{"cxx", "ii"},
+	{"CXX", "ii"},
+	{"c++", "ii"},
+	{"C++", "ii"},
+	{NULL, NULL}};
 
 /*
   something went badly wrong - just execute the real compiler
 */
 static void failed(void)
 {
+	/* delete intermediate pre-processor file if needed */
+	if (i_tmpfile) {
+		unlink(i_tmpfile);
+		free(i_tmpfile);
+		i_tmpfile = NULL;
+	}
+
+	/* delete the cpp stderr file if necessary */
+	if (cpp_stderr) {
+		unlink(cpp_stderr);
+		free(cpp_stderr);
+		cpp_stderr = NULL;
+	}
+
 	execv(orig_args->argv[0], orig_args->argv);
 	cc_log("execv returned (%s)!\n", strerror(errno));
+	perror(orig_args->argv[0]);
 	exit(1);
 }
 
@@ -62,8 +123,14 @@ static void to_cache(ARGS *args)
 
 	args_add(args, "-o");
 	args_add(args, tmp_hashname);
+	
+	if (getenv("CCACHE_CPP2")) {
+		args_add(args, input_file);
+	} else {
+		args_add(args, i_tmpfile);
+	}
 	status = execute(args->argv, tmp_stdout, tmp_stderr);
-	args_pop(args, 2);
+	args_pop(args, 3);
 
 	if (stat(tmp_stdout, &st1) != 0 || st1.st_size != 0) {
 		cc_log("compiler produced stdout for %s\n", output_file);
@@ -81,13 +148,30 @@ static void to_cache(ARGS *args)
 		stats_update(STATS_STATUS);
 
 		fd = open(tmp_stderr, O_RDONLY);
-		if (fd != -1 && 
-		    (rename(tmp_hashname, output_file) == 0 || errno == ENOENT)) {
-			/* we can use a quick method of getting the failed output */
-			copy_fd(fd, 2);
-			close(fd);
-			unlink(tmp_stderr);
-			exit(status);
+		if (fd != -1) {
+			if (strcmp(output_file, "/dev/null") == 0 ||
+			    rename(tmp_hashname, output_file) == 0 || errno == ENOENT) {
+				if (cpp_stderr) {
+					/* we might have some stderr from cpp */
+					int fd2 = open(cpp_stderr, O_RDONLY);
+					if (fd2 != -1) {
+						copy_fd(fd2, 2);
+						close(fd2);
+						unlink(cpp_stderr);
+						cpp_stderr = NULL;
+					}
+				}
+
+				/* we can use a quick method of
+                                   getting the failed output */
+				copy_fd(fd, 2);
+				close(fd);
+				unlink(tmp_stderr);
+				if (i_tmpfile) {
+					unlink(i_tmpfile);
+				}
+				exit(status);
+			}
 		}
 		
 		unlink(tmp_stderr);
@@ -121,13 +205,25 @@ static void find_hash(ARGS *args)
 {
 	int i;
 	char *path_stdout, *path_stderr;
-	char *hash_dir1;
 	char *hash_dir;
 	char *s;
 	struct stat st;
 	int status;
+	int nlevels = 2;
+
+	if ((s = getenv("CCACHE_NLEVELS"))) {
+		nlevels = atoi(s);
+		if (nlevels < 1) nlevels = 1;
+		if (nlevels > 8) nlevels = 8;
+	}
 
 	hash_start();
+
+	/* when we are doing the unifying tricks we need to include
+           the input file name in the hash to get the warnings right */
+	if (!found_debug) {
+		hash_string(input_file);
+	}
 
 	/* first the arguments */
 	for (i=1;i<args->argc;i++) {
@@ -135,9 +231,6 @@ static void find_hash(ARGS *args)
 		   theory is that these arguments will change the
 		   output of -E if they are going to have any effect
 		   at all, or they only affect linking */
-		if (strcmp(args->argv[i], input_file) == 0) {
-			continue;
-		}
 		if (i < args->argc-1) {
 			if (strcmp(args->argv[i], "-I") == 0 ||
 			    strcmp(args->argv[i], "-include") == 0 ||
@@ -147,13 +240,23 @@ static void find_hash(ARGS *args)
 				i++;
 				continue;
 			}
-			if (strncmp(args->argv[i], "-I", 2) == 0 ||
-			    strncmp(args->argv[i], "-L", 2) == 0 ||
-			    strncmp(args->argv[i], "-D", 2) == 0 ||
-			    strncmp(args->argv[i], "-isystem", 8) == 0) {
-				continue;
-			}
 		}
+		if (strncmp(args->argv[i], "-I", 2) == 0 ||
+		    strncmp(args->argv[i], "-L", 2) == 0 ||
+		    strncmp(args->argv[i], "-D", 2) == 0 ||
+		    strncmp(args->argv[i], "-isystem", 8) == 0) {
+			continue;
+		}
+
+		if (strncmp(args->argv[i], "--specs=", 8) == 0 &&
+		    stat(args->argv[i]+8, &st) == 0) {
+			/* if given a explicit specs file, then hash that file, but
+			   don't include the path to it in the hash */
+			hash_file(args->argv[i]+8);
+			continue;
+		}
+
+		/* all other arguments are included in the hash */
 		hash_string(args->argv[i]);
 	}
 
@@ -168,12 +271,14 @@ static void find_hash(ARGS *args)
 	hash_int(st.st_mtime);
 
 	/* now the run */
-	x_asprintf(&path_stdout, "%s/tmp.stdout.%d", cache_dir, getpid());
-	x_asprintf(&path_stderr, "%s/tmp.stderr.%d", cache_dir, getpid());
+	x_asprintf(&path_stdout, "%s/tmp.stdout.%d.%s", cache_dir, getpid(), 
+		   i_extension);
+	x_asprintf(&path_stderr, "%s/tmp.cpp_stderr.%d", cache_dir, getpid());
 
 	args_add(args, "-E");
+	args_add(args, input_file);
 	status = execute(args->argv, path_stdout, path_stderr);
-	args_pop(args, 1);
+	args_pop(args, 2);
 
 	if (status != 0) {
 		unlink(path_stdout);
@@ -187,38 +292,53 @@ static void find_hash(ARGS *args)
 	   preprocessor output, which means we are sensitive to line number
 	   information. Otherwise we can discard line number info, which makes
 	   us less sensitive to reformatting changes 
+
+	   Note! I have now disabled the unification code by default
+	   as it gives the wrong line numbers for warnings. Pity.
 	*/
-	if (found_debug || getenv("CCACHE_NOUNIFY")) {
+	if (found_debug || !getenv("CCACHE_UNIFY")) {
 		hash_file(path_stdout);
 	} else {
 		if (unify_hash(path_stdout) != 0) {
+			stats_update(STATS_ERROR);
 			failed();
 		}
 	}
 	hash_file(path_stderr);
 
-	unlink(path_stdout);
-	unlink(path_stderr);
-	free(path_stdout);
-	free(path_stderr);
+	i_tmpfile = path_stdout;
 
-	/* we use a 2 level subdir for the cache path to reduce the impact
+	if (!getenv("CCACHE_CPP2")) {
+		/* if we are using the CPP trick then we need to remember this stderr
+		   data and output it just before the main stderr from the compiler
+		   pass */
+		cpp_stderr = path_stderr;
+	} else {	
+		unlink(path_stderr);
+		free(path_stderr);
+	}
+
+	/* we use a N level subdir for the cache path to reduce the impact
 	   on filesystems which are slow for large directories
 	*/
 	s = hash_result();
-	x_asprintf(&hash_dir1, "%s/%c", cache_dir, s[0]);
-	x_asprintf(&stats_file, "%s/stats", hash_dir1);
-	if (create_dir(hash_dir1) != 0) {
-		cc_log("failed to create %s\n", hash_dir1);
-		failed();
+	x_asprintf(&hash_dir, "%s/%c", cache_dir, s[0]);
+	x_asprintf(&stats_file, "%s/stats", hash_dir);
+	for (i=1; i<nlevels; i++) {
+		char *p;
+		if (create_dir(hash_dir) != 0) {
+			cc_log("failed to create %s\n", hash_dir);
+			failed();
+		}
+		x_asprintf(&p, "%s/%c", hash_dir, s[i]);
+		free(hash_dir);
+		hash_dir = p;
 	}
-	x_asprintf(&hash_dir, "%s/%c", hash_dir1, s[1]);
-	free(hash_dir1);
 	if (create_dir(hash_dir) != 0) {
 		cc_log("failed to create %s\n", hash_dir);
 		failed();
 	}
-	x_asprintf(&hashname, "%s/%s", hash_dir, s+2);
+	x_asprintf(&hashname, "%s/%s", hash_dir, s+nlevels);
 	free(hash_dir);
 }
 
@@ -229,7 +349,7 @@ static void find_hash(ARGS *args)
    otherwise it returns */
 static void from_cache(int first)
 {
-	int fd_stderr;
+	int fd_stderr, fd_cpp_stderr;
 	char *stderr_file;
 	int ret;
 	struct stat st;
@@ -252,11 +372,15 @@ static void from_cache(int first)
 
 	utime(stderr_file, NULL);
 
-	unlink(output_file);
-	if (getenv("CCACHE_NOLINK")) {
-		ret = copy_file(hashname, output_file);
+	if (strcmp(output_file, "/dev/null") == 0) {
+		ret = 0;
 	} else {
-		ret = link(hashname, output_file);
+		unlink(output_file);
+		if (getenv("CCACHE_NOLINK")) {
+			ret = copy_file(hashname, output_file);
+		} else {
+			ret = link(hashname, output_file);
+		}
 	}
 
 	/* the hash file might have been deleted by some external process */
@@ -281,6 +405,23 @@ static void from_cache(int first)
 	if (ret == 0) {
 		/* update the mtime on the file so that make doesn't get confused */
 		utime(output_file, NULL);
+	}
+
+	/* get rid of the intermediate preprocessor file */
+	if (i_tmpfile) {
+		unlink(i_tmpfile);
+		free(i_tmpfile);
+		i_tmpfile = NULL;
+	}
+
+	/* send the cpp stderr, if applicable */
+	fd_cpp_stderr = open(cpp_stderr, O_RDONLY);
+	if (fd_cpp_stderr != -1) {
+		copy_fd(fd_cpp_stderr, 2);
+		close(fd_cpp_stderr);
+		unlink(cpp_stderr);
+		free(cpp_stderr);
+		cpp_stderr = NULL;
 	}
 
 	/* send the stderr */
@@ -317,6 +458,10 @@ static void find_compiler(int argc, char **argv)
 		orig_args->argv++;
 		orig_args->argc--;
 		free(base);
+		if (strchr(argv[1],'/')) {
+			/* a full path was given */
+			return;
+		}
 		base = basename(argv[1]);
 	}
 
@@ -382,27 +527,28 @@ static void find_compiler(int argc, char **argv)
 }
 
 
-/* check a filename for C/C++ extension */
-static int check_extension(const char *fname)
+/* check a filename for C/C++ extension. Return the pre-processor
+   extension */
+static char *check_extension(const char *fname)
 {
-	char *extensions[] = {"c", "C", "m", "cc", "CC", "cpp", "CPP", "cxx", "CXX", 0};
 	int i;
 	char *p, *q;
 
 	p = strrchr(fname, '.');
-	if (!p) return -1;
-	if (p <= fname) return -1;
+	if (!p) return NULL;
+	if (p <= fname) return NULL;
 
 	q = p - 1;
 	while (q > fname && *q != '.') q--;
 	if (*q == '.') q++;
 
 	p++;
-	for (i=0; extensions[i]; i++) {
-		if (strcmp(p, extensions[i]) == 0) return 0;
-		if (strncmp(q, extensions[i], p - q - 1) == 0) return 0;
+	for (i=0; extensions[i].extension; i++) {
+		if (strcmp(p, extensions[i].extension) == 0 || strncmp(q, extensions[i].extension, p - q - 1) == 0)) {
+			return extensions[i].i_extension;
+		}
 	}
-	return -1;
+	return NULL;
 }
 
 
@@ -423,9 +569,24 @@ static void process_args(int argc, char **argv)
 
 	for (i=1; i<argc; i++) {
 		/* some options will never work ... */
-		if (strncmp(argv[i], "-E", 2) == 0 ||
-		    strncmp(argv[i], "-M", 2) == 0) {
+		if (strncmp(argv[i], "-E", 2) == 0) {
 			failed();
+		}
+
+		/* cope with -MD, -MM, -MMD before the code below chucks them */
+		if (strcmp(argv[i], "-MD") == 0 ||
+		    strcmp(argv[i], "-MM") == 0 ||
+		    strcmp(argv[i], "-MMD") == 0) {
+			args_add(stripped_args, argv[i]);
+			continue;
+		}
+
+		/* check for bad options */
+		if (strncmp(argv[i], "-M", 2) == 0) {
+			cc_log("argument %s is unsupported\n", argv[i]);
+			stats_update(STATS_UNSUPPORTED);
+			failed();
+			continue;
 		}
 
 		/* we must have -c */
@@ -451,6 +612,12 @@ static void process_args(int argc, char **argv)
 			}
 			output_file = argv[i+1];
 			i++;
+			continue;
+		}
+		
+		/* alternate form of -o, with no space */
+		if (strncmp(argv[i], "-o", 2) == 0) {
+			output_file = &argv[i][2];
 			continue;
 		}
 
@@ -498,14 +665,25 @@ static void process_args(int argc, char **argv)
 		}
 
 		if (input_file) {
-			cc_log("multiple input files (%s and %s)\n",
-			       input_file, argv[i]);
-			stats_update(STATS_LINK);
+			if (check_extension(argv[i])) {
+				cc_log("multiple input files (%s and %s)\n",
+				       input_file, argv[i]);
+				stats_update(STATS_MULTIPLE);
+			} else if (!found_c_opt) {
+				cc_log("called for link with %s\n", argv[i]);
+				if (strstr(argv[i], "conftest.")) {
+					stats_update(STATS_CONFTEST);
+				} else {
+					stats_update(STATS_LINK);
+				}
+			} else {
+				cc_log("non C/C++ file %s\n", argv[i]);
+				stats_update(STATS_NOTC);
+			}
 			failed();
 		}
 
 		input_file = argv[i];
-		args_add(stripped_args, argv[i]);
 	}
 
 	if (!input_file) {
@@ -514,7 +692,8 @@ static void process_args(int argc, char **argv)
 		failed();
 	}
 
-	if (check_extension(input_file) != 0) {
+	i_extension = check_extension(input_file);
+	if (i_extension == NULL) {
 		cc_log("Not a C/C++ file - %s\n", input_file);
 		stats_update(STATS_NOTC);
 		failed();
@@ -522,7 +701,13 @@ static void process_args(int argc, char **argv)
 
 	if (!found_c_opt) {
 		cc_log("No -c option found for %s\n", input_file);
-		stats_update(STATS_LINK);
+		/* I find that having a separate statistic for autoconf tests is useful,
+		   as they are the dominant form of "called for link" in many cases */
+		if (strstr(input_file, "conftest.")) {
+			stats_update(STATS_CONFTEST);
+		} else {
+			stats_update(STATS_LINK);
+		}
 		failed();
 	}
 
@@ -547,7 +732,7 @@ static void process_args(int argc, char **argv)
 	}
 
 	/* cope with -o /dev/null */
-	if (stat(output_file, &st) == 0 && !S_ISREG(st.st_mode)) {
+	if (strcmp(output_file,"/dev/null") != 0 && stat(output_file, &st) == 0 && !S_ISREG(st.st_mode)) {
 		cc_log("Not a regular file %s\n", output_file);
 		stats_update(STATS_DEVICE);
 		failed();
@@ -575,7 +760,7 @@ static void ccache(int argc, char *argv[])
 	/* if we can return from cache at this point then do */
 	from_cache(1);
 	
-	/* run real compiler, semding output to cache */
+	/* run real compiler, sending output to cache */
 	to_cache(stripped_args);
 
 	/* return from cache */
